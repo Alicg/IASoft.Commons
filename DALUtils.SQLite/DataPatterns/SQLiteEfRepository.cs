ea934fc9b12812a83dc36c4d9445d9d100b405fc
dmitry.panchenko@aspose.com <dmitry.panchenko@aspose.com>
2015-11-22 23:05:47 +0100

    DAL utils for SQLite added.

---
+++ DALUtils.SQLite/DataPatterns/SQLiteEfRepository.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+
+namespace DALUtils.SQLite.DataPatterns
+{
+    using Utils.DAL.BaseEntities;
+    using Utils.DAL.DataPatterns.EfDataPatterns;
+
+    public class SQLiteEfRepository<T> : EfRepository<T> where T : BaseEntity, new()
+    {
+        public SQLiteEfRepository(EfUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+            unitOfWork.Context.Database.ExecuteSqlCommand("PRAGMA foreign_keys = ON;");
+        }
+
+        public override void Add(T entity)
+        {
+            if (entity.IsNew())
+                entity.Id = this.GetNextId();
+            base.Add(entity);
+        }
+
+        public override void AddOrUpdate(T entity)
+        {
+            if (entity.IsNew())
+                entity.Id = this.GetNextId();
+            base.AddOrUpdate(entity);
+        }
+        
+        private static int lastDbId;
+        private static int lastSetId;
+
+        private int GetNextId()
+        {
+            lock (this.GetType())
+            {
+                int dbId;
+                try
+                {
+                    dbId = this.All().Max(v => v.Id);
+                }
+                catch
+                {
+                    dbId = 0;
+                }
+                if (dbId == lastDbId)
+                {
+                    lastSetId++;
+                    return lastSetId;
+                }
+                lastDbId = dbId;
+                return lastSetId = dbId + 1;
+            }
+        }
+    }
+}
