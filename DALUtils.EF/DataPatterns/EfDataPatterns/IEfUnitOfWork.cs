3ddd1eec09e435e8ff2eca82d844359610950c80
dmitry.panchenko@aspose.com <dmitry.panchenko@aspose.com>
2015-11-22 22:38:05 +0100

    DALUtils for EntityFramework added.

---
+++ DALUtils.EF/DataPatterns/EfDataPatterns/IEfUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace Utils.DAL.DataPatterns.EfDataPatterns
+{
+    using System.Data.Entity;
+
+    public interface IEfUnitOfWork : IUnitOfWork
+    {
+        DbContext Context { get; set; }
+    }
+}
---
+++ DALUtils.EF/EfExtensions/EfExtensions.cs
@@ -0,0 +1,46 @@
+﻿namespace Utils.DAL.EfExtensions
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+    using System.Linq;
+    using System.Linq.Expressions;
+
+    using Utils.DAL.BaseEntities;
+    using Utils.Extensions;
+
+    public static class EfExtensions
+    {
+        public static T Update<T>(this IDbSet<T> dbSet, T entity, bool? ignoreNulls = null) where T : BaseEntity
+        {
+            ignoreNulls = ignoreNulls ?? true;
+            T result;
+            if(!TryUpdate(dbSet, entity, out result, ignoreNulls))
+                throw new Exception($"Не удалось найти элемент '{dbSet.ElementType.Name}' по указанному коду: {entity.Id}");
+            return result;
+        }
+
+        public static bool TryUpdate<T>(this IDbSet<T> dbSet, T entity, out T result, bool? ignoreNulls = null) where T : BaseEntity
+        {
+            ignoreNulls = ignoreNulls ?? true;
+            result = dbSet.Find(entity.Id);
+            if (result != null)
+            {
+                entity.CopyAllTo(result, ignoreNulls.Value);
+                return true;
+            }
+            return false;
+        }
+
+        public static IQueryable<T> Includes<T>(this IQueryable<T> source, params Expression<Func<T, object>>[] paths)
+            where T : class
+        {
+            return paths.Aggregate(source, (current, path) => current.Include(path));
+        }
+
+        public static string GetTableName<T>(this DbContext context) where T : class
+        {
+            return (context as IObjectContextAdapter).ObjectContext.CreateObjectSet<T>().EntitySet.Name;
+        }
+    }
+}
