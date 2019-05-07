24584079cea20bfa826daab457b7a533a76350de
Dmitry Panchenko <panchenko.dm@gmail.com>
2019-05-07 09:22:33 +0200

    Обновил версию ffmpeg.

--- Video/FFMpegExecutable/FFMpegExeLoader.cs
+++ Video/FFMpegExecutable/FFMpegExeLoader.cs
@@ -7,16 +7,16 @@ namespace FFMpegExecutable
         public static string UnpackFFMpegExe()
         {
             var thisAssembly = typeof(FFMpegExeLoader).Assembly;
-            ResourcesUtils.UnpackResource(thisAssembly, "FFMpegExecutable", "avcodec-57.dll");
-            ResourcesUtils.UnpackResource(thisAssembly, "FFMpegExecutable", "avdevice-57.dll");
-            ResourcesUtils.UnpackResource(thisAssembly, "FFMpegExecutable", "avfilter-6.dll");
-            ResourcesUtils.UnpackResource(thisAssembly, "FFMpegExecutable", "avformat-57.dll");
-            ResourcesUtils.UnpackResource(thisAssembly, "FFMpegExecutable", "avutil-55.dll");
+            ResourcesUtils.UnpackResource(thisAssembly, "FFMpegExecutable", "avcodec-58.dll");
+            ResourcesUtils.UnpackResource(thisAssembly, "FFMpegExecutable", "avdevice-58.dll");
+            ResourcesUtils.UnpackResource(thisAssembly, "FFMpegExecutable", "avfilter-7.dll");
+            ResourcesUtils.UnpackResource(thisAssembly, "FFMpegExecutable", "avformat-58.dll");
+            ResourcesUtils.UnpackResource(thisAssembly, "FFMpegExecutable", "avutil-56.dll");
             ResourcesUtils.UnpackResource(thisAssembly, "FFMpegExecutable", "ffplay.exe");
             ResourcesUtils.UnpackResource(thisAssembly, "FFMpegExecutable", "ffprobe.exe");
-            ResourcesUtils.UnpackResource(thisAssembly, "FFMpegExecutable", "postproc-54.dll");
-            ResourcesUtils.UnpackResource(thisAssembly, "FFMpegExecutable", "swresample-2.dll");
-            ResourcesUtils.UnpackResource(thisAssembly, "FFMpegExecutable", "swscale-4.dll");
+            ResourcesUtils.UnpackResource(thisAssembly, "FFMpegExecutable", "postproc-55.dll");
+            ResourcesUtils.UnpackResource(thisAssembly, "FFMpegExecutable", "swresample-3.dll");
+            ResourcesUtils.UnpackResource(thisAssembly, "FFMpegExecutable", "swscale-5.dll");
             return ResourcesUtils.UnpackResource(thisAssembly, "FFMpegExecutable", "ffmpeg.exe");
         }
     }
